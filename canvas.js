967cf3500d361850887504645010d842b1294acf
Karla <karla.ruan@cceo.com.mx>
2018-05-15 12:07:43 -0500

    Sombras

---
+++ canvas.js
@@ -0,0 +1,7 @@
+const canvas=document.getElementById("canvas");
+const context=canvas.getContext('2d');
+
+/*
+Agregar texto es con: strokeText y con fillText (" ",coordX,coordY);
+*/
+context.fillText("Hola mundo",500,100);
